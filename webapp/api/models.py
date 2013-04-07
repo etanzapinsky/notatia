19df806a57f6947c86bb57bf12b36f85efced0af
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-06 21:22:39 -0400

    added capsule models

--- webapp/api/models.py
+++ webapp/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Capsule(models.Model):
+    authors = models.ManyToManyField(User)
+    tags = models.ManyToManyField(Tag, blank=True)
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    path = models.CharField(max_length=1024)
+    last_modified = models.DateField(auto_now=True)
+    first_created = models.DateField(auto_now_add=True)
