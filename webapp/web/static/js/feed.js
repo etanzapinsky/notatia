559217e275c32bc02b30b4ae40f04c1bdbe0f537
Gil Chen-Zion <gc2466@columbia.edu>
2013-04-22 13:31:27 -0400

    Backbone

--- webapp/web/static/js/feed.js
+++ webapp/web/static/js/feed.js
@@ -1,4 +1,47 @@
+var Capsules = Backbone.Collection.extend({
+	url: '/api/filter/capsules/'
+});
 
+var CapsuleList = Backbone.View.extend({
+	el: '.page',
+	render: function(){
+		var that = this;
+		var capsules = new Capsules();
+		capsules.fetch({
+			success: function(capsules){
+				var template = _.template($('#capsule-list-template').html(), {capsules: capsules.models})
+				that.$el.html(template);
+			}
+		});
+		
+	}
+});
+
+
+
+var Router = Backbone.Router.extend({
+	routes: {
+		'': 'home'
+	}
+});
+
+
+$(document).ready(function() {
+
+var capsuleList = new CapsuleList();
+var router = new Router();
+router.on('route:home', function() {
+	capsuleList.render();
+});
+
+Backbone.history.start();
+
+});
+
+
+
+
+/*
 var Capsule = Backbone.Model.extend({
     urlRoot: '/api/capsule/',
     defaults: {
@@ -14,16 +57,30 @@ var Capsules = Backbone.Collection.extend({
 	url: '/api/filter/capsules/'
 });
 
+var CapsulesView = Backbone.View.extend({
+	tagName: 'div',
+	id: 'capsuleList',
+	render: function(){
+		this.el.innerHTML = this.model.get("0").attributes.title;
+		return this;
+	}
+});
+
 
 $(document).ready(function() {
-
+	
 	var capsules = new Capsules();
 
 	capsules.on("reset", function() {
+		var cv = new CapsulesView();
+		cv.render();
 		this.each(function(capsule) {
-			console.log(capsule);
+			console.log(capsule.attributes);
+			
 		});
 	});
 
 	capsules.fetch({reset: true});
+		
 });
+*/
