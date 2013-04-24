23a006a5a2f54f20c1f79df3c91a2a7a73c3b749
Gil Chen-Zion <gc2466@columbia.edu>
2013-04-23 23:53:34 -0400

    Update work on backbone edit PUT still not working

--- webapp/web/static/js/feed.js
+++ webapp/web/static/js/feed.js
@@ -1,3 +1,103 @@
+var Capsules = Backbone.Collection.extend({
+	url: '/api/filter/capsules/'
+});
+
+var Capsule = Backbone.Model.extend({
+	urlRoot: '/api/capsule/',
+    defaults: {
+        title: '',
+        text: '',
+        path: '',
+        tags: ''
+    }
+});
+
+var Router = Backbone.Router.extend({
+	routes: {
+		'': 'home',
+		'new': 'editCapsule',
+		'edit/:id': 'editCapsule'
+	}
+});
+
+
+
+$(document).ready(function() {
+	
+var CapsuleList = Backbone.View.extend({
+		el: '.page',
+		render: function(){
+			var that = this;
+			var capsules = new Capsules();
+			capsules.fetch({
+				success: function(capsules){
+					var template = _.template($('#capsule-list-template').html(), {capsules: capsules.models})
+					that.$el.html(template);
+				}
+			});
+		
+		}
+});
+
+var EditCapsule = Backbone.View.extend({
+		el: '.page',
+		render: function(options){
+			var that = this;
+			debugger;
+			if(options.id){
+				//GET REQUEST
+				var capsule = new Capsule({id: options.id});
+				capsule.fetch({
+					success: function(capsule){
+						var template = _.template($('#edit-capsule-template').html(), {capsule: capsule});
+						that.$el.html(template);
+					}
+				});
+			} else {
+				var template = _.template($('#edit-capsule-template').html(), {capsule: null});
+				this.$el.html(template);
+			}
+			
+		
+		}, 
+		events:{ 'submit .edit-capsule-form': 'saveCapsule' },
+		saveCapsule:function (ev)
+		{ var capsuleDetails = $(ev.currentTarget).serializeObject(); 
+		var capsule = new Capsule(); 
+		capsule.save( capsuleDetails, 
+		{ 
+			success: function(capsule){
+				router.navigate('', {trigger: true}); 
+			}
+		});
+		return false;
+		}
+});
+
+
+var router = new Router();
+
+var capsuleList = new CapsuleList();
+var editCapsule = new EditCapsule();
+
+
+router.on('route:home', function() {
+	capsuleList.render();
+});
+
+router.on('route:editCapsule', function(id) {
+	editCapsule.render({id: id});
+	$('#tags').tagsInput({
+	    'width':'488px',
+	    'height': '50px',
+	    'placeholderColor' : '#999'
+	});
+});
+
+Backbone.history.start();
+
+});
+
 $.fn.serializeObject = function() {
       var o = {};
       var a = this.serializeArray();
@@ -13,137 +113,3 @@ $.fn.serializeObject = function() {
       });
       return o;
 };
-
-
-var Capsules = Backbone.Collection.extend({
-	url: '/api/filter/capsules/'
-});
-
-var Capsule = Backbone.Model.extend({
-	urlRoot: '/api/capsule/',
-    defaults: {
-        title: '',
-        text: '',
-        path: '',
-        tags: ''
-    }
-});
-
-var CapsuleList = Backbone.View.extend({
-	el: '.page',
-	render: function(){
-		var that = this;
-		var capsules = new Capsules();
-		capsules.fetch({
-			success: function(capsules){
-				var template = _.template($('#capsule-list-template').html(), {capsules: capsules.models})
-				that.$el.html(template);
-			}
-		});
-		
-	}
-});
-
-var EditCapsule = Backbone.View.extend({
-	el: '.page',
-	render: function(){
-		var template = _.template($('#edit-capsule-template').html(), {})
-		this.$el.html(template);
-		
-	},
-	events: {
-		'submit .edit-capsule-form': 'saveCapsule'
-	},
-	saveCapsule: function (ev){
-		var capsuleDetails = $(ev.currentTarget).serializeObject();
-		var capsule = new Capsule();
-		capsule.save({
-			title: capsuleDetails.title,
-			text: capsuleDetails.text,
-			tags: capsuleDetails.tags
-		}, {
-			success: function(){
-				console.log(capsule.toJSON());
-			}
-			
-		});
-		console.log(capsuleDetails);
-		return false;
-	}
-});
-
-
-
-var Router = Backbone.Router.extend({
-	routes: {
-		'': 'home',
-		'new': 'editCapsule'
-	}
-});
-
-
-$(document).ready(function() {
-
-var capsuleList = new CapsuleList();
-var editCapsule = new EditCapsule();
-
-var router = new Router();
-
-router.on('route:home', function() {
-	capsuleList.render();
-});
-
-router.on('route:editCapsule', function() {
-	editCapsule.render();
-});
-
-Backbone.history.start();
-
-});
-
-
-
-
-/*
-var Capsule = Backbone.Model.extend({
-    urlRoot: '/api/capsule/',
-    defaults: {
-        title: '',
-        text: '',
-        path: '',
-        tags: ''
-    }
-});
-
-var Capsules = Backbone.Collection.extend({
-	model: Capsule,
-	url: '/api/filter/capsules/'
-});
-
-var CapsulesView = Backbone.View.extend({
-	tagName: 'div',
-	id: 'capsuleList',
-	render: function(){
-		this.el.innerHTML = this.model.get("0").attributes.title;
-		return this;
-	}
-});
-
-
-$(document).ready(function() {
-	
-	var capsules = new Capsules();
-
-	capsules.on("reset", function() {
-		var cv = new CapsulesView();
-		cv.render();
-		this.each(function(capsule) {
-			console.log(capsule.attributes);
-			
-		});
-	});
-
-	capsules.fetch({reset: true});
-		
-});
-*/
