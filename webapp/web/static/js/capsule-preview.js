0cab765e15ba0de10f37e7e4c1a1e7c6b5f7d902
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-29 20:24:37 -0400

    highlighted inner link not really working

--- webapp/web/static/js/capsule-preview.js
+++ webapp/web/static/js/capsule-preview.js
@@ -13,40 +13,43 @@ $(document).ready(function() {
     ProperCapsuleView = FriendCapsuleView;
     if (capsule) {
         var main_capsule = new MainCapsuleView({model: capsule});
+        capsule.view = main_capsule;
         $(main_capsule.el).attr('id', 'main-capsule');
         $('#main-body').prepend(main_capsule.el);
     }
     else {
         var main_capsule = new MainCapsuleView({model: new Capsule()});
+        capsule.view = main_capsule;
         main_capsule.template = main_capsule.edit_template;
         main_capsule.render();
         $(main_capsule.el).attr('id', 'main-capsule');
         $('#main-body').prepend(main_capsule.el);
     }
 
-    var link_ids = capsule.attributes.links;
-    for (var i = 0; i < link_ids.length; i++) {
+    var links = capsule.attributes.links;
+    for (var i = 0; i < links.length; i++) {
         $.ajax({
             type: 'GET',
-            url: '/api/link/' + link_ids[i].id,
+            url: '/api/capsule/' + links[i].capsule,
             data: {},
             success: function(data, status, jqXHR) {
-                $.ajax({
-                    type: 'GET',
-                    url: '/api/capsule/' + data.capsule,
-                    data: {},
-                    success: function(data, status, jqXHR) {
-                        var view = new FriendCapsuleView({model: new Capsule(data)});
-                        view.original_template = view.template;
-                        view.template = view.linked_template;
-                        view.render();
-                        $(view.el).appendTo($('#main-stream'));
-                    },
-                    error: function(jqXHR, status, error) {
-                        console.log(error);
-                    }
-                    
-                });
+                var view = new FriendCapsuleView({model: new Capsule(data)});
+                view.original_template = view.template;
+                view.template = view.linked_template;
+                view.render();
+                var match;
+                for (var j = 0; j < links.length; j++) {
+                    if (links[j].capsule == data.id)
+                        match = links[j];
+                }
+                if (match.left == null) {
+                    $(view.el).appendTo($('#main-stream'));
+                }
+                else {
+                    var t = capsule.attributes.text;
+                    var new_html = t.slice(0, match.left) + '<span class="previewable">' + t.slice(match.left, match.width) + '</span>' + t.slice(match.width);
+                    $('#main-capsule-body').html(new_html);
+                }
             },
             error: function(jqXHR, status, error) {
                 console.log(error);
