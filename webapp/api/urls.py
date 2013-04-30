3746fa6c304b2b77b2e412b58bf552485920005d
Etan Zapinsky <etan.zapinsky@gmail.com>
2013-04-30 01:05:53 -0400

    fixed view problems

--- webapp/api/urls.py
+++ webapp/api/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import patterns, url, include
 
-from api import views
-
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='api_home'),
-    url(r'^create/tag/$', views.create_tag, name='create_tag'),
-    url(r'^recent_capsules/$', views.recent_capsules, name='recent_capsules'),
-    url(r'^capsule/(?P<capsule_id>\d*)$', views.process_capsule, name='capsule'),
-    url(r'^filter/capsules/$', views.filter_capsules, name='filter_capsules'),
-    url(r'^author/(?P<username>[\w.@+-]+)$', views.get_author, name='author'),
-    url(r'^search/', views.search, name='search'),
-    url(r'link/(?P<from_id>\d+)/(?P<to_id>\d+)$', views.create_link, name='create_link'),
-    url(r'link/(?P<pk>\d+)$', views.get_link, name='get_link'),
+    url(r'^$', 'api.views.index', name='api_home'),
+    url(r'^create/tag/$', 'api.views.create_tag', name='create_tag'),
+    url(r'^recent_capsules/$', 'api.views.recent_capsules', name='recent_capsules'),
+    url(r'^capsule/(?P<capsule_id>\d*)$', 'api.views.process_capsule', name='capsule'),
+    url(r'^filter/capsules/$', 'api.views.filter_capsules', name='filter_capsules'),
+    url(r'^author/(?P<username>[\w.@+-]+)$', 'api.views.get_author', name='author'),
+    url(r'^search/', 'api.views.search', name='search'),
+    url(r'link/(?P<from_id>\d+)/(?P<to_id>\d+)$', 'api.views.create_link', name='create_link'),
+    url(r'link/(?P<pk>\d+)$', 'api.views.get_link', name='get_link'),
 )
